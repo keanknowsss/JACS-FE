3bde0d70d2e3c8e93ae805ab5b4e0ce825a5f724
rextumlos <johnrex412@gmail.com>
2022-11-23 13:58:17 +0800

    Added initial template for each carousel card

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+export {default as BuildCarousel} from './build-carousel.png';
+export {default as FooterLogo} from './footer logo.png';
+export {default as LearnCarousel} from './learn-carousel.png';
+export {default as RepairCarousel} from './repair-carousel.png';
+export {default as ShopCarousel} from './shop-carousel.png';
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { Footer } from "../components/Footer";
 import { HomeCarousel } from "../components/Carousel";
@@ -10,28 +10,27 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex flex-col h-screen">
-        <main className="flex flex-col lg:flex-row justify-between items-center bg-background w-full h-full p-3">
- 
-          <div className="bg-container w-full h-screen flex-auto px-3 pt-3 md:pb-3 rounded-t-md md:rounded-l-md md:rounded-tr-none">
-            <div className="bg-background w-full h-full rounded-md">
-              <HomeCarousel />
-            </div>
+      <main className="flex flex-col lg:flex-row justify-between items-center bg-background w-full h-auto p-3">
+        <div className="bg-container w-full h-screen flex-auto px-3 pt-3 lg:pb-3 rounded-t-md lg:rounded-l-md lg:rounded-tr-none">
+          <div className="bg-background w-full h-full rounded-md">
+            <HomeCarousel />
           </div>
+        </div>
 
-          <div className="flex flex-col flex-auto justify-evenly items-center bg-container w-full h-screen py-3 pr-3 pl-3 md:pl-0 rounded-b-md md:rounded-r-md md:rounded-bl-none">
-            <div className="bg-background w-full h-full mb-3 rounded-md">
-              Title
-            </div>
-            <div className="bg-background w-full h-full mb-3 rounded-md">
-              Most Bought Items
-            </div>
-            <div className="bg-background w-full h-full rounded-md">
-              Trending Builds
-            </div>
+        <div className="flex flex-col flex-auto justify-evenly items-center bg-container w-full h-screen py-3 pr-3 pl-3 lg:pl-0 rounded-b-md lg:rounded-r-md lg:rounded-bl-none">
+          <div className="bg-background w-full h-full mb-3 rounded-md">
+            Title
           </div>
-        </main>
+          <div className="bg-background w-full h-full mb-3 rounded-md">
+            Most Bought Items
+          </div>
+          <div className="bg-background w-full h-full rounded-md">
+            Trending Builds
+          </div>
+        </div>
+      </main>
 
+      <div className="block left-0 top-0">
         <Footer />
       </div>
     </>
