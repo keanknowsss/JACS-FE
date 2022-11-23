e72e6bfed45c5a2cae53332faacc94527f9a15f5
rextumlos <johnrex412@gmail.com>
2022-11-23 11:25:19 +0800

    Added responsive layout for Homepage
    - Created HomeCarousel.js
    - Added assets to be used for HomeCarousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,41 @@
 import { useEffect } from "react";
 
-import { Footer } from "../components/Footer"
+import { Footer } from "../components/Footer";
+import { HomeCarousel } from "../components/Carousel";
 
 const Home = () => {
-    useEffect(() => {
-        document.title = "JACS Home";
-    }, [])
+  useEffect(() => {
+    document.title = "JACS Home";
+  }, []);
 
-	return (
-		<>
-			<div className="flex flex-col h-screen bg-background">
-				<main className="flex flex-row justify-between items-center w-full h-full p-3">
+  return (
+    <>
+      <div className="flex flex-col h-screen">
+        <main className="flex flex-col lg:flex-row justify-between items-center bg-background w-full h-full p-3">
+ 
+          <div className="bg-container w-full h-screen flex-auto px-3 pt-3 md:pb-3 rounded-t-md md:rounded-l-md md:rounded-tr-none">
+            <div className="bg-background w-full h-full rounded-md">
+              <HomeCarousel />
+            </div>
+          </div>
 
-					<div className="bg-gray-800 w-full h-full ">
-						Carousel
-					</div>
+          <div className="flex flex-col flex-auto justify-evenly items-center bg-container w-full h-screen py-3 pr-3 pl-3 md:pl-0 rounded-b-md md:rounded-r-md md:rounded-bl-none">
+            <div className="bg-background w-full h-full mb-3 rounded-md">
+              Title
+            </div>
+            <div className="bg-background w-full h-full mb-3 rounded-md">
+              Most Bought Items
+            </div>
+            <div className="bg-background w-full h-full rounded-md">
+              Trending Builds
+            </div>
+          </div>
+        </main>
 
-					<div className="flex flex-col bg-gray-800 w-full h-full ">
-						<div className="">
-							Title
-						</div>
-						<div className="">
-							Most Bought Items
-						</div>
-						<div className="">
-							Trending Builds
-						</div>
-					</div>
-
-				</main>
-				<Footer />
-			</div>			
-		</>
-	);
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default Home;
