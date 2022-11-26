a4939c869182e0f82d71091ad7a18c91be73f6c4
rextumlos <johnrex412@gmail.com>
2022-11-26 20:08:16 +0800

    Enhanced responsive layout for Homepage
    - Used grid instead of flex for layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import React, { useEffect } from "react";
 import { Footer } from "../components/Footer";
 import { HomeCarousel } from "../components/Carousel";
 import { HomeCarouselData } from "../utils";
+import { ProductCard } from "../components/Card";
+
+import { JACSLogo } from "../assets/images";
 
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination"
+import "swiper/css/pagination";
 import "swiper/css/thumbs";
-import { ProductCard } from "../components/Card";
 
 const Home = () => {
   useEffect(() => {
@@ -17,23 +19,22 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex flex-col">
-        <main className="flex flex-col lg:flex-row h-full lg:h-screen justify-between items-center bg-background p-3">
-          <div className="flex-1 bg-container w-full lg:w-64 h-full px-3 pt-3 lg:pb-3 rounded-t-md lg:rounded-l-md lg:rounded-tr-none">
+        <main className="px-5 py-3 grid grid-cols-1 lg:grid-cols-4 grid-rows-3 gap-3 w-full h-full justify-center items-center bg-container box-border">
+          <section aria-label="home carousel" className="col-span-1 lg:col-span-2 row-span-3 w-full h-full rounded-t-md lg:rounded-l-md lg:rounded-tr-none">
             <div className="bg-background w-full h-screen lg:h-full shadow-md rounded-md">
               <HomeCarousel categories={HomeCarouselData} />
             </div>
+          </section>
+
+          <div className="col-span-1 lg:col-span-2 row-span-1 p-3 lg:p-0 bg-background w-full h-full flex justify-center items-center rounded-md">
+            <JACSLogo />
           </div>
 
-          <div className="flex flex-col flex-1 w-full lg:w-64 h-screen lg:h-full justify-evenly items-center bg-container py-3 pr-3 pl-3 lg:pl-0 lg:rounded-r-md lg:rounded-bl-none  rounded-b-md">
-            <div className="bg-background w-full h-full mb-3 rounded-md">
-              Title
-            </div>
-            <div className="bg-background w-full h-full mb-3 rounded-md p-3">
-              <p className="text-white text-2xl font-serif font-bold py-3 pl-3">
+          <div className="col-span-1 lg:col-span-2 row-span-1 bg-background w-full h-full rounded-md">
+            <p className="text-white text-2xl font-serif font-bold py-3 pl-3">
               Most Bought items
-              </p>
-            <div className="bg-container rounded-md flex flex-row flex-auto justify-evenly items-center">
+            </p>
+            <div className="rounded-md flex flex-row justify-center items-center">
               <div>
                 <ProductCard />
               </div>
@@ -45,11 +46,12 @@ const Home = () => {
               </div>
             </div>
           </div>
-            <div className="bg-background w-full h-full mb-3 rounded-md p-3">
-              <p className="text-white text-3xl font-serif font-bold py-3 pl-3">
+
+          <div className="col-span-1 lg:col-span-2 row-span-1 bg-background w-full h-full rounded-md">
+            <p className="text-white text-3xl font-serif font-bold py-3 pl-3">
               Trending items
             </p>
-            <div className="bg-container rounded-md flex flex-row flex-auto justify-evenly items-center">
+            <div className="rounded-md flex flex-row justify-center items-center">
               <div>
                 <ProductCard />
               </div>
@@ -59,17 +61,12 @@ const Home = () => {
               <div>
                 <ProductCard />
               </div>
-            </div>
             </div>
           </div>
-  
+
         </main>
         <Footer />
-      </div>
 
-      {/* <div className="block left-0 top-0">
-        <Footer />
-      </div> */}
     </>
   );
 };
