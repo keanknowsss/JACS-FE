3d93f115ab3adacff0c1d78a9efe93f57b13f898
Phillip Angelo Jaculo <parjaculo228922@gmail.com>
2022-12-14 13:31:50 +0800

    filter contents layout

---
+++ src/components/ShopModal/Modal.jsx
@@ -0,0 +1,78 @@
+import FilterModalScss from "./FilterModal.module.scss"
+
+const Modal = ({open, closeModal}) => {
+    if(!open) return null;
+  return (
+    <div className={FilterModalScss.overlay}>
+        <div className={FilterModalScss.modalContainer}>
+            <p onClick={closeModal} className={FilterModalScss.closeBtn}>
+                X
+            </p>
+                <div className={FilterModalScss.filterContainer}>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                    <div className={FilterModalScss.filter}>Test</div>
+                </div>
+                <div className={FilterModalScss.confirmBtn}>
+                    Confirm
+                </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Modal
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { ProductCard } from "../components/Card";
 import { PaginationShop } from "../components/PaginationShop";
 import { ShopSlider } from "../components/PriceRangeSlider";
+import { FilterModal } from "../components/ShopModal";
 import { SearchBar } from "../components/ShopSearch";
 
 const Shop = () => {
@@ -21,25 +22,7 @@ const Shop = () => {
           </div>
           <div></div>
           <div className="relative">
-            <div className="p-3 text-secondary-default justify-center items-center mb-2 bg-primary-default rounded w-[100px] h-[50px] absolute bottom-[-9px] right-0"> 
-              <div className="flex justify-center items-center bottom-[8px]">
-                <svg
-                  class="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
-                  ></path>
-                </svg>
-                <span className="text-white">Filter</span>
-              </div>
-            </div>
+            <FilterModal/>
           </div>
         </div>
         <div class="my-3 h-px bg-secondary-default "></div>
