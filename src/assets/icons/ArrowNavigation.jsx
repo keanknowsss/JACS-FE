6044b2f187266e8c9e7f89f6b38384b0fa7b2591
keanknowsss <solomon.keanu.sk@gmail.com>
2023-03-24 11:17:37 +0800

    Fixes on Development #3: Icons and Other Components

--- src/assets/icons/ArrowNavigation.jsx
+++ src/assets/icons/ArrowNavigation.jsx
@@ -1,14 +1,13 @@
-const ArrowNavigation = ({ className, fill, onClick }) => {
+const ArrowNavigation = ({ className }) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			viewBox="0 0 24 24"
 			className={className}
-			onClick={onClick}
 		>
 			<path
 				d="m10 19a1 1 0 0 1 -.64-.23 1 1 0 0 1 -.13-1.41l4.48-5.36-4.32-5.37a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6a1 1 0 0 1 -.83.36z"
-				fill={fill ? fill : "#231f20"}
+				fill="currentColor"
 			/>
 		</svg>
 	);
--- src/assets/icons/HeartIcon.jsx
+++ src/assets/icons/HeartIcon.jsx
@@ -1,14 +1,12 @@
-const HeartIcon = ({className, active}) => {
+const HeartIcon = ({className }) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			className={className}
-			width="24"
-			height="24"
 			viewBox="0 0 24 24"
 			strokeWidth="2"
-			stroke="#F66B0E"
-			fill="#333840"
+			stroke="currentColor"
+			fill="none"
 		>
 			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 			<path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
