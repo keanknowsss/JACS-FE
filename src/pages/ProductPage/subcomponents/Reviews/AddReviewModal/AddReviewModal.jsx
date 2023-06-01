7f2adad70a13e1d5e845ff744ad5462f22a63df1
keanknowsss <solomon.keanu.sk@gmail.com>
2023-06-01 12:07:09 +0800

    fixed review input

--- src/pages/ProductPage/subcomponents/Reviews/AddReviewModal/AddReviewModal.jsx
+++ src/pages/ProductPage/subcomponents/Reviews/AddReviewModal/AddReviewModal.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect, useState, memo } from "react";
 import { useSelector } from "react-redux";
 import { AddIcon } from "../../../../../assets/icons";
 import StarRating from "../../../../../components/StarRating/StarRating";
@@ -6,29 +6,70 @@ import styles from "./AddReviewModal.module.scss";
 
 import { getUserDetail } from "../../../../../features/api/builders/userApi";
 import {
-  selectCurrentToken,
-  selectCurrentUserId,
+	selectCurrentToken,
+	selectCurrentUserId,
 } from "../../../../../features/slice/userAccessSlice";
 
+const AddForm = React.memo(
+	({ rating, setRating, description, setDescription, handleSubmit }) => {
+		const handleDescription = (e) => {
+			setDescription(e.target.value);
+		};
+
+		return (
+			<>
+				<div className={styles.rating}>
+					<span>Rating: </span>
+					<StarRating
+						modify={true}
+						onRating={(rate) => setRating(rate)}
+						rating={rating}
+					/>
+				</div>
+
+				<form
+					className={styles.addForm}
+					onSubmit={handleSubmit}
+				>
+					<label>
+						<span className={styles.labelDes}>Description:</span>
+						<textarea
+							className={styles.description}
+							placeholder="Write a description..."
+							value={description}
+							onChange={handleDescription}
+						></textarea>
+					</label>
+					<button className={styles.addReviewBtn}>Add review</button>
+				</form>
+			</>
+		);
+	}
+);
+
+const NotLoggedIn = () => {
+	return <p>Please log in first.</p>;
+};
+
 const AddReviewModal = ({ referenceID }) => {
-  const [modal, setModal] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [queryData] = getUserDetail.useLazyQuery();
+	const [modal, setModal] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [queryData] = getUserDetail.useLazyQuery();
 
-  const [description, setDescription] = useState('');
-  const [userID, setUserID] = useState(null);
-  const [rating, setRating] = useState(0);
+	const [description, setDescription] = useState("");
+	const [userID, setUserID] = useState(null);
+	const [rating, setRating] = useState(0);
 
-  const token = useSelector(selectCurrentToken);
+	const token = useSelector(selectCurrentToken);
 	const id = useSelector(selectCurrentUserId);
-  
-  useLayoutEffect(() => {
+
+	useLayoutEffect(() => {
 		const getData = async () => {
 			if (token && id) {
 				const { data, error } = await queryData(id);
 				if (!error) {
 					setIsLoggedIn(true);
-          setUserID(data?.result?.id);
+					setUserID(data?.result?.id);
 				} else {
 					console.log("error in profile name", error?.result);
 				}
@@ -37,69 +78,59 @@ const AddReviewModal = ({ referenceID }) => {
 		getData();
 	}, [token, id]);
 
-  const toggleModal = () => {
-    setModal(!modal);
-  };
+	const toggleModal = () => {
+		setModal(!modal);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({
-      userID,
-      referenceID,
-      rating,
-      description,
-    })
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+    toggleModal();
+		console.log({
+			userID,
+			referenceID,
+			rating,
+			description,
+		});
+	};
 
-  const AddForm = () => {
-    return (
-      <>
-        <div className={styles.rating}>
-          <span>Rating: </span>
-          <StarRating
-            modify={true}
-            onRating={(rate) => setRating(rate)}
-            rating={rating}
-          />
-        </div>
+	return (
+		<>
+			<button
+				className={styles.addBtn}
+				onClick={toggleModal}
+			>
+				<AddIcon className={styles.addIcon} />
+				Add a Review
+			</button>
 
-        <form className={styles.addForm} onSubmit={handleSubmit}>
-          <label>
-            <span className={styles.labelDes}>Description:</span>
-            <textarea className={styles.description} placeholder="Write a description..." value={description} onChange={e => setDescription(e.target.value)}></textarea>
-          </label>
-          <button className={styles.addReviewBtn}>Add review</button>
-        </form>
-      </>
-    );
-  };
+			{modal && (
+				<div className={styles.modal}>
+					<div className={styles.modalContent}>
+						<h2>Add a Review</h2>
+						<span className={styles.divider}></span>
 
-  const NotLoggedIn = () => {
-    return <p>Please log in first.</p>;
-  };
-
-  return (
-    <>
-      <button className={styles.addBtn} onClick={toggleModal}>
-        <AddIcon className={styles.addIcon} />
-        Add a Review
-      </button>
-
-      {modal && (
-        <div className={styles.modal}>
-          <div className={styles.modalContent}>
-            <h2>Add a Review</h2>
-            <span className={styles.divider}></span>
-
-            {isLoggedIn ? <AddForm /> : <NotLoggedIn />}
-            <button className={styles.closeBtn} onClick={toggleModal}>
-              Close
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-  );
+						{isLoggedIn ? (
+							<AddForm
+								rating={rating}
+								setRating={setRating}
+								description={description}
+								setDescription={setDescription}
+								handleSubmit={handleSubmit}
+							/>
+						) : (
+							<NotLoggedIn />
+						)}
+						<button
+							className={styles.closeBtn}
+							onClick={toggleModal}
+						>
+							Close
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default AddReviewModal;
