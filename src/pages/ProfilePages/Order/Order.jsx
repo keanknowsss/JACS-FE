fe79db5b67318bc48981b45ea59538f454811604
keanknowsss <solomon.keanu.sk@gmail.com>
2023-06-07 18:19:36 +0800

    Added My Orders Page

--- src/pages/ProfilePages/Order/Order.jsx
+++ src/pages/ProfilePages/Order/Order.jsx
@@ -1,8 +1,26 @@
+import styles from "./Order.module.scss";
+import StoreRow from "./subComponents/StoreRow";
 
-const Order = () => {
-  return (
-    <div>Order</div>
-  )
-}
+const Order = ({ title }) => {
+	document.title = title;
 
-export default Order
+	// FOR TEST
+	const STORE_NAME = ["PCX", "Silicon Volley"];
+
+	return (
+		<main className={styles.orderPage}>
+			<section className={styles.itemsContainer}>
+				<div className={styles.storeItemsContainer}>
+          {STORE_NAME.map((name) => (
+            <StoreRow
+              name={name}
+              checked={null}
+            />
+          ))}
+        </div>
+			</section>
+		</main>
+	);
+};
+
+export default Order;
